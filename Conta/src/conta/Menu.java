0ddc73d4601b5a358832d173d03495541cb81def
CatianeNascimento <csnascimento07@gmail.com>
2023-02-23 16:32:57 -0300

    criação da classe conta

--- Conta/src/conta/Menu.java
+++ Conta/src/conta/Menu.java
@@ -2,17 +2,28 @@ package conta;
 
 import java.util.Scanner;
 
+import conta.model.Conta;
+import conta.util.Cores;
+
 public class Menu {
 
 	public static void main(String[] args) {
 
-		Scanner input = new Scanner(System.in); 
+		Scanner input = new Scanner(System.in);
+		
+		Conta c1 = new Conta(1, 123, 1, "Adriana", 10000.0f);
+		c1.Visualizar();
+		c1.Sacar(12000.0f);
+		c1.Visualizar();
+		c1.Depositar(5000.0f);
+		c1.Visualizar();
 		
 		int opcao;
-		
-		while(true) {
-			
-			System.out.println("--------------------------------------------------");
+
+		while (true) {
+
+			System.out.println(Cores.TEXT_YELLOW + Cores.ANSI_BLACK_BACKGROUND
+					+          "--------------------------------------------------");
 			System.out.println("                                                  ");
 			System.out.println("           BANCO DO BRAZIL COM Z                  ");
 			System.out.println("                                                  ");
@@ -24,64 +35,61 @@ public class Menu {
 			System.out.println("   5 - Apagar Conta                               ");
 			System.out.println("   6 - Sacar                                      ");
 			System.out.println("   7 - Depositar                                  ");
-			System.out.println("   8-  Transferir valores entre Contas            ");
+			System.out.println("   8 - Transferir valores entre Contas           ");
 			System.out.println("   9 - Sair                                       ");
 			System.out.println("                                                  ");
 			System.out.println("--------------------------------------------------");
 			System.out.println("Entre com a opção desejada                        ");
-			System.out.println("                                                  ");
+			System.out.println("                                                  " + Cores.TEXT_RESET);
 			opcao = input.nextInt();
-			
-			switch(opcao) {
-			
+
+			switch (opcao) {
+
 			case 1:
-				System.out.println("Criar Conta");
+				System.out.println(Cores.TEXT_WHITE_BOLD + "Criar Conta");
 				break;
-				
-			case 2: 
-				System.out.println("Listar todas as Contas");
+
+			case 2:
+				System.out.println(Cores.TEXT_WHITE_BOLD + "Listar todas as Contas");
 				break;
-				
-			case 3: 
-				System.out.println("Consultar dados da Conta - por número");
+
+			case 3:
+				System.out.println(Cores.TEXT_WHITE_BOLD + "Consultar dados da Conta - por número");
 				break;
-				
-			case 4: 
-				System.out.println("Atualizar dados da Conta");
+
+			case 4:
+				System.out.println(Cores.TEXT_WHITE_BOLD + "Atualizar dados da Conta");
 				break;
-				
-			case 5: 
-				System.out.println("Apagar Conta");
+
+			case 5:
+				System.out.println(Cores.TEXT_WHITE_BOLD + "Apagar Conta");
 				break;
-				
-			case 6: 
-				System.out.println("Saque");
+
+			case 6:
+				System.out.println(Cores.TEXT_WHITE_BOLD + "Saque");
 				break;
-				
-			case 7: 
-				System.out.println("Déposito");
+
+			case 7:
+				System.out.println(Cores.TEXT_WHITE_BOLD + "Déposito");
 				break;
-				
-			case 8: 
-				System.out.println("Transferência entre Contas");
+
+			case 8:
+				System.out.println(Cores.TEXT_WHITE_BOLD + "Transferência entre Contas");
 				break;
-				
-			case 9: 
-				System.out.println("Banco do Brazil com Z - O seu futuro começa aqui");
+
+			case 9:
+				System.out.println(Cores.TEXT_WHITE_BOLD + "Banco do Brazil com Z - O seu futuro começa aqui");
 				input.close();
 				System.exit(0);
 				break;
-				
-				default: 
-					System.out.println("Opção Inválida");
-					break;
+
+			default:
+				System.out.println(Cores.TEXT_RED_BOLD + "Opção Inválida");
+				break;
 			}
 
-
-
-
 		}
-		
+
 	}
 
 }
